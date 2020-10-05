c24ec25b92716a5f368a33199d219e2cd1607241
daekit <65988431+daekit@users.noreply.github.com>
2020-10-05 14:08:23 +0900

    사용자 등록시 직급 추가

--- src/main/java/com/dksys/biz/admin/level/service/LevelService.java
+++ src/main/java/com/dksys/biz/admin/level/service/LevelService.java
@@ -5,14 +5,14 @@ import java.util.Map;
 
 public interface LevelService {
 
-	public List<Map<String, String>> selectLevelList();
+	public List<Map<String, String>> selectLevelList(Map<String, String> paramMap);
 
 	public int selectLevelCount(Map<String, String> paramMap);
 
-	public void insertLevel(Map<String, String> paramMap);
+	public void insertLevel(Map<String, String> paramMap) throws Exception;
 
 	public Map<String, String> selectLevelInfo(Map<String, String> paramMap);
 
-	public void updateLevel(Map<String, String> paramMap);
+	public void updateLevel(Map<String, String> paramMap) throws Exception;
 
 }
