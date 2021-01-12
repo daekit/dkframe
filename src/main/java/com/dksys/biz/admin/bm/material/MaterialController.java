c541f80e5d8f65e40781245c6bd8461b89586207
KJK <KJK@10.90.4.137>
2021-01-12 16:41:46 +0900

    자재코드 관리 스켈레톤 완성

--- src/main/java/com/dksys/biz/admin/bm/material/MaterialController.java
+++ src/main/java/com/dksys/biz/admin/bm/material/MaterialController.java
@@ -1,11 +1,32 @@
 package com.dksys.biz.admin.bm.material;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dksys.biz.admin.bm.material.service.MaterialService;
+import com.dksys.biz.util.MessageUtils;
 
 @Controller
 @RequestMapping("/admin/bm/material")
 public class MaterialController {
 
+	@Autowired
+	MessageUtils messageUtils;
     
+    @Autowired
+    MaterialService materialService;
+	
+	@PostMapping(value = "/selectMaterialList")
+    public String selectMaterialList(ModelMap model) {
+		List<Map<String, String>> materialList = materialService.selectMaterialList();
+		model.addAttribute("materialList", materialList);
+        return "jsonView";
+    }
+	
 }
---
+++ src/main/java/com/dksys/biz/admin/bm/material/service/MaterialService.java
@@ -0,0 +1,11 @@
+package com.dksys.biz.admin.bm.material.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface MaterialService {
+
+    public List<Map<String, String>> selectMaterialList();
+
+
+}
