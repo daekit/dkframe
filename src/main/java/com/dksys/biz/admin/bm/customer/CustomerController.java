e470044a9452798701a5e218a6121baebe040a55
kbg <kbg@DESKTOP-R5PTGSO>
2021-01-14 16:54:01 +0900

    ㄴ

--- src/main/java/com/dksys/biz/admin/bm/customer/CustomerController.java
+++ src/main/java/com/dksys/biz/admin/bm/customer/CustomerController.java
@@ -1,11 +1,32 @@
 package com.dksys.biz.admin.bm.customer;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dksys.biz.admin.bm.material.service.MaterialService;
+import com.dksys.biz.util.MessageUtils;
 
 @Controller
 @RequestMapping("/admin/bm/customer")
 public class CustomerController {
 
+	@Autowired
+	MessageUtils messageUtils;
+    
+    @Autowired
+    MaterialService materialService;
+	
+	@PostMapping(value = "/selectCustomerList")
+    public String selectCustomerList(ModelMap model) {
+		List<Map<String, String>> materialList = materialService.selectMaterialList();
+		model.addAttribute("materialList", materialList);
+        return "jsonView";
+    }
     
 }
