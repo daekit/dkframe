a14c9eea74606628afa0fb90efada8ac796e7a5b
daekit <daeki91@gmail.com>
2021-01-25 13:40:02 +0900

    거래처 마스터관리 첫번째 커밋

--- src/main/java/com/dksys/biz/admin/bm/bm01/BM01Ctr.java
+++ src/main/java/com/dksys/biz/admin/bm/bm01/BM01Ctr.java
@@ -1,33 +1,10 @@
-package com.dksys.biz.admin.bm.customer;
+package com.dksys.biz.admin.bm.bm01;
 
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.dksys.biz.admin.bm.material.service.MaterialService;
-import com.dksys.biz.util.MessageUtils;
-
 @Controller
-@RequestMapping("/admin/bm/customer")
-public class CustomerController {
+@RequestMapping("/admin/bm/bm01")
+public class BM01Ctr {
 
-	@Autowired
-	MessageUtils messageUtils;
-    
-    @Autowired
-    MaterialService materialService;
-	
-	@PostMapping(value = "/selectCustomerList")
-    public String selectCustomerList(@RequestBody Map<String, String> param, ModelMap model) {
-		List<Map<String, String>> materialList = materialService.selectMaterialList(param);
-		model.addAttribute("materialList", materialList);
-        return "jsonView";
-    }
-    
 }
