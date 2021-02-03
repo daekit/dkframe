85b5d1773b51f0fc85ead03edf44539e9b9cbbf7
daekit <daeki91@gmail.com>
2021-02-03 14:37:52 +0900

    거래처 마스터 관리 커밋

--- src/main/java/com/dksys/biz/admin/bm/bm02/BM02Ctr.java
+++ src/main/java/com/dksys/biz/admin/bm/bm02/BM02Ctr.java
@@ -1,17 +1,19 @@
 package com.dksys.biz.admin.bm.bm02;
 
+import java.util.List;
 import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import com.dksys.biz.admin.bm.bm02.service.BM02Svc;
+import com.dksys.biz.cmn.vo.PaginationInfo;
 import com.dksys.biz.util.MessageUtils;
 
 @Controller
@@ -21,17 +23,31 @@ public class BM02Ctr {
 	@Autowired
 	MessageUtils messageUtils;
 	
+	@Autowired
+	BM02Svc bm02Svc;
+	
 	@PostMapping("/insertClnt")
-    public String insertClnt(@RequestParam Map<String, String> paramMap, HttpServletRequest request, MultipartHttpServletRequest mRequest, ModelMap model) {
+    public String insertClnt(@RequestParam Map<String, String> paramMap, MultipartHttpServletRequest mRequest, ModelMap model) {
     	try {
-    		paramMap.put("pgmId", request.getRequestURI());
+    		bm02Svc.insertClnt(paramMap, mRequest);
     		model.addAttribute("resultCode", 200);
     		model.addAttribute("resultMessage", messageUtils.getMessage("insert"));
     	}catch(Exception e){
     		model.addAttribute("resultCode", 500);
     		model.addAttribute("resultMessage", messageUtils.getMessage("fail"));
     	}
-    	
     	return "jsonView";
+    }
+	
+	// 공통코드 리스트 조회
+    @PostMapping("/selectClntList")
+    public String selectClntList(@RequestBody Map<String, String> param, ModelMap model) {
+    	int totalCnt = bm02Svc.selectClntCount(param);
+    	PaginationInfo paginationInfo = new PaginationInfo(param, totalCnt);
+    	model.addAttribute("paginationInfo", paginationInfo);
+    	
+    	List<Map<String, String>> clntList = bm02Svc.selectClntList(param);
+    	model.addAttribute("clntList", clntList);
+        return "jsonView";
     }
 }
--- src/main/java/com/dksys/biz/admin/bm/bm02/service/BM02Svc.java
+++ src/main/java/com/dksys/biz/admin/bm/bm02/service/BM02Svc.java
@@ -1,5 +1,16 @@
 package com.dksys.biz.admin.bm.bm02.service;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
 public interface BM02Svc {
+
+	void insertClnt(Map<String, String> paramMap, MultipartHttpServletRequest mRequest);
+
+	int selectClntCount(Map<String, String> param);
+
+	List<Map<String, String>> selectClntList(Map<String, String> param);
 
 }
