1d4c3696ee50246b241197d0afd6c6ed868e6fbe
jungkit <power.kimjungki@gmail.com>
2021-03-08 17:52:10 +0900

    매출실적관리 진행중

--- src/main/java/com/dksys/biz/user/ar/ar02/AR02Ctr.java
+++ src/main/java/com/dksys/biz/user/ar/ar02/AR02Ctr.java
@@ -1,11 +1,38 @@
 package com.dksys.biz.user.ar.ar02;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dksys.biz.cmn.vo.PaginationInfo;
+import com.dksys.biz.user.ar.ar02.service.AR02Svc;
+import com.dksys.biz.util.MessageUtils;
 
 @Controller
 @RequestMapping("/user/ar/ar02")
 public class AR02Ctr {
  
+	@Autowired
+	MessageUtils messageUtils;
+    
+    @Autowired
+    AR02Svc ar02Svc;
+	
+    @PostMapping(value = "/selectSellList")
+	public String selectSellList(@RequestBody Map<String, String> paramMap, ModelMap model) {
+    	int totalCnt = ar02Svc.selectSellCount(paramMap);
+		PaginationInfo paginationInfo = new PaginationInfo(paramMap, totalCnt);
+    	model.addAttribute("paginationInfo", paginationInfo);
+    	
+    	List<Map<String, String>> resultList = ar02Svc.selectSellList(paramMap);
+    	model.addAttribute("resultList", resultList);
+    	return "jsonView";
+	}
     
 }
