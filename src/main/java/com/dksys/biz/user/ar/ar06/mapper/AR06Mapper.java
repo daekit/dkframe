d34bb3e3b24755d2912dcadd781e504acfc83f68
'jinho1875' <jinho8882@hanmail.net>
2021-03-16 13:34:44 +0900

    2021-03-16 정진호 세금계산서 담당자 관리

---
+++ src/main/java/com/dksys/biz/user/ar/ar06/mapper/AR06Mapper.java
@@ -0,0 +1,23 @@
+package com.dksys.biz.user.ar.ar06.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AR06Mapper {
+	
+	int selectTaxInvoiceCount(Map<String, String> paramMap);
+
+	List<Map<String, String>> selectTaxInvoiceList(Map<String, String> paramMap);
+	
+	Map<String, String> selectTaxInvoiceInfo(Map<String, String> paramMap);
+	
+	int insertTaxInvoice(Map<String, String> param);
+
+	int updateTaxInvoice(Map<String, String> param);
+	
+	int deleteTaxInvoice(Map<String, String> param);
+	
+}
