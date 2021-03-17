57115be88e603d2f21523d20f45ec94ab254bf6d
dkkil <kilssengkyu@gmail.com>
2021-03-17 16:30:38 +0900

    commit for conflict

---
+++ src/main/java/com/dksys/biz/user/ar/ar04/mapper/AR04Mapper.java
@@ -0,0 +1,23 @@
+package com.dksys.biz.user.ar.ar04.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AR04Mapper {
+
+	int selectTaxBilgCount(Map<String, String> paramMap);
+
+	List<Map<String, String>> selectTaxBilgList(Map<String, String> paramMap);
+
+	Map<String, String> selectTaxBilg(Map<String, String> paramMap);
+
+	int updateTaxBilg(Map<String, String> paramMap);
+
+	int selectTaxRcvCount(Map<String, String> paramMap);
+
+	List<Map<String, String>> selectTaxRcvList(Map<String, String> paramMap);
+
+}
