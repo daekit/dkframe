8a1616858654585fe5077a1ec75d1b9c5b2c57dd
dkkil <kilssengkyu@gmail.com>
2021-03-19 07:43:48 +0900

    대시보드 개발중

---
+++ src/main/java/com/dksys/biz/admin/cm/cm10/mapper/CM10Mapper.java
@@ -0,0 +1,19 @@
+package com.dksys.biz.admin.cm.cm10.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CM10Mapper {
+	
+	int selectShipNCount(Map<String, String> paramMap);
+
+	List<Map<String, String>> selectShipNList(Map<String, String> paramMap);
+	
+	int selectOrdrgNCount(Map<String, String> paramMap);
+
+	List<Map<String, String>> selectOrdrgNList(Map<String, String> paramMap);
+
+}
---
+++ src/main/java/com/dksys/biz/admin/cm/cm10/service/CM10Svc.java
@@ -0,0 +1,16 @@
+package com.dksys.biz.admin.cm.cm10.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CM10Svc {
+
+	public List<Map<String, String>> selectShipNList(Map<String, String> param);
+	
+	public int selectShipNCount(Map<String, String> param);
+
+	public List<Map<String, String>> selectOrdrgNList(Map<String, String> param);
+	
+	public int selectOrdrgNCount(Map<String, String> param);
+	
+}
