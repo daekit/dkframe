e17e2caa8abaf0690e2dc44eb7bf08ca3f7d0b0b
'jinho1875' <jinho8882@hanmail.net>
2021-03-23 15:37:38 +0900

    2021-03-23 정진호 매입/매출집계

---
+++ src/main/java/com/dksys/biz/user/sd/sd02/SD02Ctr.java
@@ -0,0 +1,100 @@
+package com.dksys.biz.user.sd.sd02;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dksys.biz.cmn.vo.PaginationInfo;
+import com.dksys.biz.user.sd.sd02.service.SD02Svc;
+import com.dksys.biz.util.MessageUtils;
+
+
+@Controller
+@RequestMapping("/user/sd/sd02")
+public class SD02Ctr {
+ 
+	//단가 관리 컨트롤러 UNIT PRICE -> UPR 표기
+	
+	@Autowired
+	MessageUtils messageUtils;
+	
+    @Autowired
+    SD02Svc sd02svc;
+    
+    @PostMapping(value = "/selectSellList")
+    public String selectSellList(@RequestBody Map<String, String> param, ModelMap model) {
+    	int totalCnt = sd02svc.selectSellCount(param);
+    	PaginationInfo paginationInfo = new PaginationInfo(param, totalCnt);
+    	model.addAttribute("paginationInfo", paginationInfo);
+    
+    	List<Map<String, String>> sellList = sd02svc.selectSellList(param);
+    	model.addAttribute("sellList", sellList);
+        return "jsonView";
+    }
+    
+    @PostMapping(value = "/selectPlanInfo")
+    public String selectPlanInfo(@RequestBody Map<String, String> paramMap, ModelMap model) {
+    	Map<String, String> result = sd02svc.selectPlanInfo(paramMap);
+    	model.addAttribute("result", result);
+    	return "jsonView";
+    }
+    
+    @PostMapping(value = "/insertPlan")
+    public String insertPlan(HttpServletRequest request, @RequestBody Map<String, String> param, ModelMap model) {
+    	try {
+	    	sd02svc.insertPlan(param);
+	    	model.addAttribute("resultCode", 200);
+	    	model.addAttribute("resultMessage", messageUtils.getMessage("insert"));
+    	}catch(Exception e) {
+    		model.addAttribute("resultCode", 500);
+    		model.addAttribute("resultMessage", messageUtils.getMessage("fail"));
+    	}
+	    	return "jsonView";
+    }
+    
+    @PutMapping(value = "/updatePlan")
+    public String updatePlan(@RequestBody Map<String, String> param, ModelMap model) {
+    	try {
+			 sd02svc.updatePlan(param);
+			 model.addAttribute("resultUpr", 200);
+			 model.addAttribute("resultMessage", messageUtils.getMessage("update"));
+    	}catch(Exception e) {
+	    	 model.addAttribute("resultCode", 500);
+	 		 model.addAttribute("resultMessage", messageUtils.getMessage("fail"));
+    	}
+    		return "jsonView";
+    }
+    
+    @PostMapping(value = "/copyInsert")
+    public String copyInsert(@RequestBody Map<String, String> param, ModelMap model) {
+    	try {
+    		sd02svc.deleteCopy(param);
+	    	sd02svc.copyInsert(param);
+	    	model.addAttribute("resultCode", 200);
+	    	model.addAttribute("resultMessage", messageUtils.getMessage("insert"));
+    	}catch(Exception e) {
+    		model.addAttribute("resultCode", 500);
+    		model.addAttribute("resultMessage", messageUtils.getMessage("fail"));
+    	}
+	    	return "jsonView";
+    }
+    
+    @DeleteMapping(value = "/deletePlan")
+    public String deleteEst(@RequestBody Map<String, String> paramMap, ModelMap model) {
+    	sd02svc.deletePlan(paramMap);
+    	model.addAttribute("resultCode", 200);
+    	model.addAttribute("resultMessage", messageUtils.getMessage("delete"));
+    	return "jsonView";
+    }
+    
+}
---
+++ src/main/java/com/dksys/biz/user/sd/sd02/service/SD02Svc.java
@@ -0,0 +1,26 @@
+package com.dksys.biz.user.sd.sd02.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+public interface SD02Svc {
+	
+	public int selectSellCount(Map<String, String> param);
+	
+	public List<Map<String, String>> selectSellList(Map<String, String> param);
+	
+	Map<String, String> selectPlanInfo(Map<String, String> paramMap);
+	
+	public int insertPlan(Map<String, String> param);
+
+	public int updatePlan(Map<String, String> param);
+	
+	public int copyInsert(Map<String, String> param);
+
+	public int deleteCopy(Map<String, String> param);
+	
+	int deletePlan(Map<String, String> paramMap);
+
+}
