7252aba8a1c72f1d8568d6a0bb5da7bf894520bf
'jinho1875' <jinho8882@hanmail.net>
2021-03-24 17:30:03 +0900

    2021-03-24 정진호 거래처 담보현황관리

---
+++ src/main/java/com/dksys/biz/admin/bm/bm08/BM08Ctr.java
@@ -0,0 +1,54 @@
+package com.dksys.biz.admin.bm.bm08;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.dksys.biz.admin.bm.bm08.service.BM08Svc;
+import com.dksys.biz.cmn.vo.PaginationInfo;
+import com.dksys.biz.util.MessageUtils;
+
+@Controller
+@RequestMapping("/admin/bm/bm08")
+public class BM08Ctr {
+	
+	@Autowired
+	MessageUtils messageUtils;
+	
+	@Autowired
+	BM08Svc bm08Svc;
+	
+	// 거래처 리스트 조회
+    @PostMapping("/selectClntPledgeList")
+    public String selectClntPledgeList(@RequestBody Map<String, String> paramMap, ModelMap model) {
+    	int totalCnt = bm08Svc.selectClntPledgeCount(paramMap);
+    	PaginationInfo paginationInfo = new PaginationInfo(paramMap, totalCnt);
+    	model.addAttribute("paginationInfo", paginationInfo);
+    	
+    	List<Map<String, String>> clntList = bm08Svc.selectClntPledgeList(paramMap);
+    	model.addAttribute("clntList", clntList);
+        return "jsonView";
+    }
+
+    // 거래처 리스트 조회
+    @PostMapping("/selectClntPledgeDetailList")
+    public String selectClntPledgeDetailList(@RequestBody Map<String, String> paramMap, ModelMap model) {
+    	int totalCnt = bm08Svc.selectClntPledgeDetailCount(paramMap);
+    	PaginationInfo paginationInfo = new PaginationInfo(paramMap, totalCnt);
+    	model.addAttribute("paginationInfo", paginationInfo);
+    	
+    	List<Map<String, String>> clntDetailList = bm08Svc.selectClntPledgeDetailList(paramMap);
+    	model.addAttribute("clntDetailList", clntDetailList);
+    	return "jsonView";
+    }
+    
+}
---
+++ src/main/java/com/dksys/biz/admin/bm/bm08/service/BM08Svc.java
@@ -0,0 +1,20 @@
+package com.dksys.biz.admin.bm.bm08.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+public interface BM08Svc {
+
+	int selectClntPledgeCount(Map<String, String> paramMap);
+
+	List<Map<String, String>> selectClntPledgeList(Map<String, String> paramMap);
+
+	int selectClntPledgeDetailCount(Map<String, String> paramMap);
+	
+	List<Map<String, String>> selectClntPledgeDetailList(Map<String, String> paramMap);
+
+	
+
+}
