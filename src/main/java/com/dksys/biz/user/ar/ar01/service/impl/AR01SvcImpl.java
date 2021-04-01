70ee7e06d66f14e24bd30cee7af724fe53188148
jungkit <power.kimjungki@gmail.com>
2021-04-01 18:10:19 +0900

    출하, 발주 오류수정

--- src/main/java/com/dksys/biz/user/ar/ar01/service/impl/AR01SvcImpl.java
+++ src/main/java/com/dksys/biz/user/ar/ar01/service/impl/AR01SvcImpl.java
@@ -189,6 +189,7 @@ public class AR01SvcImpl implements AR01Svc {
 				detailMap.put("coCd", paramMap.get("coCd"));
 				detailMap.put("clntCd", paramMap.get("clntCd"));
 				detailMap.put("selpchCd", "SELPCH2");
+				detailMap.put("prdtUpr", detailMap.get("realShipUpr"));
 				detailMap.put("rmk", paramMap.get("shipRmk"));
 				sd08Mapper.insertCplrUntpc(detailMap);
 			}
