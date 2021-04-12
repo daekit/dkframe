c6a16073f86b4bae388ff9feef2513d6bfc77d73
daekit <daeki91@gmail.com>
2021-04-12 15:21:34 +0900

    거래처 관리 수정 완료

--- src/main/java/com/dksys/biz/admin/bm/bm01/service/BM01Svc.java
+++ src/main/java/com/dksys/biz/admin/bm/bm01/service/BM01Svc.java
@@ -5,19 +5,19 @@ import java.util.Map;
 
 public interface BM01Svc {
 
+	public int selectMaterialCount(Map<String, String> param);
+	
     public List<Map<String, String>> selectMaterialList(Map<String, String> param);
-
-	public int deleteMaterial(Map<String, String> param);
+    
+    public Map<String, String> selectMaterialInfo(Map<String, String> param);
+    
+    public int selectPrdtCntInPrjct(Map<String, String> param);
+    
+    public int checkOverLap(Map<String, String> param);
 
 	public int insertMaterial(Map<String, String> param);
 	
 	public int updateMaterial(Map<String, String> param);
 
-	public int checkOverLap(Map<String, String> param);
-
-	public Map<String, String> selectMaterialInfo(Map<String, String> param);
-
-	public int selectMaterialCount(Map<String, String> param);
-
-
+	public int deleteMaterial(Map<String, String> param);
 }
