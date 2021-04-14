b547df4d463b3711fcbb3a1539da03ad653e2fa4
jungkit <power.kimjungki@gmail.com>
2021-04-14 15:21:56 +0900

    오류수정

--- src/main/java/com/dksys/biz/user/ar/ar01/service/impl/AR01SvcImpl.java
+++ src/main/java/com/dksys/biz/user/ar/ar01/service/impl/AR01SvcImpl.java
@@ -215,7 +215,7 @@ public class AR01SvcImpl implements AR01Svc {
 			detailMap = ar01Mapper.selectShipDetailInfo(detailMap);
 			paramMap.putAll(detailMap);
 			paramMap.put("trstDt", 		paramMap.get("dlvrDttm").replace("-", ""));
-			paramMap.put("estCoprt", 	detailMap.get("taxivcCoprt"));
+			paramMap.put("estCoprt", 	paramMap.get("taxivcCoprt"));
 			paramMap.put("pchsUpr", 	"0");
 			paramMap.put("sellUpr",     detailMap.get("realShipUpr"));
 			paramMap.put("stockUpr",    detailMap.get("stockUpr"));
