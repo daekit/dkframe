4b894b2d946910de816fcd0c7b8afa50800c0d5a
dkkil <kilssengkyu@gmail.com>
2021-04-22 09:30:13 +0900

    mes 매출 반영시 출하 확정 로직 추가

--- src/main/java/com/dksys/biz/user/ar/ar01/service/impl/AR01SvcImpl.java
+++ src/main/java/com/dksys/biz/user/ar/ar01/service/impl/AR01SvcImpl.java
@@ -182,6 +182,7 @@ public class AR01SvcImpl implements AR01Svc {
 		return result;
 	}
 
+	//출하확정 수정시 updateConfirmToMes도 같이 수정해주어야함.
 	@Override
 	public int updateConfirm(Map<String, String> paramMap) {
 		//마감 체크
@@ -274,6 +275,121 @@ public class AR01SvcImpl implements AR01Svc {
 					int stockQty = Integer.parseInt(stockInfo.get("stockQty")) - Integer.parseInt(detailMap.get("realShipQty"));
 					paramMap.put("stockQty", String.valueOf(stockQty));
 				}
+				sm01Mapper.updateStockSell(paramMap);
+			}
+		}
+		// 여신 체크
+		paramMap.put("realTotTrstAmt", String.valueOf(realTotTrstAmt));
+		paramMap.put("clntCd",  clntCd);
+		if(ar02Svc.checkLoan(paramMap)) {
+			TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+			return 0;
+		}
+		if(selectConfirmCount(paramMap) == selectDetailCount(paramMap)) {
+			ar01Mapper.updateConfirm(paramMap);
+		}
+		return result;
+	}
+	
+	@Override
+	public int updateConfirmToMes(Map<String, String> paramMap, List<Map<String, String>> detailList) {
+		//마감 체크
+		int result = 0;
+		int realTotTrstAmt = 0;
+		result = detailList.size();
+		String clntCd = paramMap.get("clntCd");
+		String clntNm = paramMap.get("clntNm");
+		String userId = paramMap.get("loginId");
+		String pgmId = paramMap.get("pgmId");
+		for(Map<String, String> detailMap : detailList) {
+			detailMap.put("login_Id", userId);
+			detailMap.put("ship_Proc_Id", userId);
+			detailMap.put("user_id", userId);
+			detailMap.put("pgm_id", pgmId);
+			detailMap.put("ship_Seq", paramMap.get("shipSeq"));
+			//커플러일 경우 별도 단가 데이터 저장
+			if(detailMap.get("prdtStkn") != null) {
+				if(detailMap.get("prdt_Stkn").contains("PDSKCP")) {
+					detailMap.put("co_Cd", paramMap.get("coCd"));
+					detailMap.put("clnt_Cd", paramMap.get("clntCd"));
+					detailMap.put("selpch_Cd", "SELPCH2");
+					detailMap.put("prdt_Dt", paramMap.get("reqDt"));
+					detailMap.put("prdt_Upr", detailMap.get("realShipUpr"));
+					detailMap.put("rmk", paramMap.get("shipRmk"));
+					sd08Mapper.insertCplrUntpc(detailMap);
+				}
+			}
+			ar01Mapper.updateConfirmDetail(detailMap);
+			//매출정보 insert
+			detailMap = ar01Mapper.selectShipDetailInfo(detailMap);
+			paramMap.putAll(detailMap);
+			paramMap.put("trstDt", 		paramMap.get("dlvrDttm").replace("-", "").substring(0,8));
+			paramMap.put("estCoprt", 	paramMap.get("taxivcCoprt"));
+			paramMap.put("pchsUpr", 	"0");
+			paramMap.put("sellUpr",     detailMap.get("realShipUpr"));
+			paramMap.put("stockUpr",    detailMap.get("stockUpr"));
+			paramMap.put("trstQty",     detailMap.get("shipQty"));
+			paramMap.put("trstWt",      detailMap.get("shipWt"));
+			paramMap.put("trstUpr",     detailMap.get("shipUpr"));
+			paramMap.put("trstAmt",     detailMap.get("shipAmt"));
+			paramMap.put("realTrstQty", detailMap.get("realShipQty"));
+			paramMap.put("realTrstWt",  detailMap.get("realShipWt"));
+			paramMap.put("realTrstUpr", detailMap.get("realShipUpr"));
+			paramMap.put("realTrstAmt", detailMap.get("realShipAmt"));
+			realTotTrstAmt += Integer.parseInt(detailMap.get("realShipAmt"));
+			paramMap.put("bilgQty",     detailMap.get("realShipQty"));
+			paramMap.put("bilgWt",      detailMap.get("realShipWt"));
+			paramMap.put("bilgUpr",     detailMap.get("realShipUpr"));
+			paramMap.put("bilgAmt",     detailMap.get("realShipAmt"));
+			paramMap.put("clntCd",      clntCd);
+			paramMap.put("clntNm",      clntNm);
+			paramMap.put("prdtSpec", 	detailMap.get("prdtSpec"));	
+			paramMap.put("prdtSize", 	detailMap.get("prdtSize"));		
+			paramMap.put("trspTypCd", 	"TRSPTYP1");              /* 정상매출 */
+			paramMap.put("trstRprcSeq", detailMap.get("shipSeq"));		
+			paramMap.put("trstDtlSeq", 	detailMap.get("shipDtlSeq"));				  	
+			paramMap.put("odrNo", 		paramMap.get("odrSeq"));
+			paramMap.put("trspRmk", 	paramMap.get("shipRmk"));
+			paramMap.put("selpchCd", "SELPCH2"); // 매출데이터
+			long bilgVatAmt = ar02Mapper.selectBilgVatAmt(paramMap);
+			paramMap.put("bilgVatAmt", 	String.valueOf(bilgVatAmt));
+			realTotTrstAmt += bilgVatAmt;
+			paramMap.put("mngTel", "");
+			paramMap.put("userId", userId);
+			paramMap.put("pgmId", pgmId);
+			ar02Mapper.insertPchsSell(paramMap);
+			
+			if(detailMap.containsKey("prdtStockCd") && "Y".equals(detailMap.get("prdtStockCd").toString())) 
+			{
+				// 재고정보 update
+				// 구분이 자사의 경우 재고추체=거래처는 금문으로 변경
+				if("OWNER1".equals(paramMap.get("ownerCd").toString())) {					
+					paramMap.put("clntCd",  paramMap.get("whClntCd"));		
+				}
+				paramMap.put("clntCd", clntCd);
+				paramMap.put("clntNm", clntNm);
+				
+				paramMap.put("prdtCd", detailMap.get("prdtCd"));
+				paramMap.put("prdtSize", detailMap.get("prdtSize"));
+				paramMap.put("prdtSpec", detailMap.get("prdtSpec"));
+				paramMap.put("prdtLen", detailMap.get("prdtLen"));
+				Map<String, String> stockInfo = sm01Mapper.selectStockInfo(paramMap);
+				paramMap.put("stockChgCd", "STOCKCHG02");
+				if(stockInfo == null) {
+					paramMap.put("pchsUpr", detailMap.get("realShipUpr"));
+					paramMap.put("sellUpr", detailMap.get("realShipUpr"));
+					paramMap.put("stockUpr", detailMap.get("realShipUpr"));
+					paramMap.put("stdUpr", detailMap.get("realShipUpr"));
+					paramMap.put("stockQty", "-"+detailMap.get("realShipQty"));
+				} else {
+					paramMap.put("pchsUpr", stockInfo.get("pchsUpr"));
+					paramMap.put("sellUpr", detailMap.get("realShipUpr"));
+					paramMap.put("stockUpr", stockInfo.get("stockUpr"));
+					paramMap.put("stdUpr", stockInfo.get("stdUpr"));
+					int stockQty = Integer.parseInt(stockInfo.get("stockQty")) - Integer.parseInt(detailMap.get("realShipQty"));
+					paramMap.put("stockQty", String.valueOf(stockQty));
+				}
+				paramMap.put("impYn", "N");
 				sm01Mapper.updateStockSell(paramMap);
 			}
 		}
