e84ce4f6e19e400d6e7982eda214be897bd5a5ac
byungchulan <choolab@gmail.com>
2021-05-17 16:08:53 +0900

    @Transactional  추가

--- src/main/java/com/dksys/biz/user/od/od02/service/impl/OD02SvcImpl.java
+++ src/main/java/com/dksys/biz/user/od/od02/service/impl/OD02SvcImpl.java
@@ -5,11 +5,13 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dksys.biz.user.od.od02.mapper.OD02Mapper;
 import com.dksys.biz.user.od.od02.service.OD02Svc;
 
 @Service
+@Transactional("erpTransactionManager")
 public class OD02SvcImpl implements OD02Svc {
     
 	@Autowired
