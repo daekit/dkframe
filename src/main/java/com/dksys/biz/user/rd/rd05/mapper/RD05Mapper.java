17334369b722ff46678de7602862d99e3011cb91
byungchulan <choolab@gmail.com>
2021-06-11 11:22:03 +0900

    수불관리(기준일자) 최초생성

---
+++ src/main/java/com/dksys/biz/user/rd/rd05/mapper/RD05Mapper.java
@@ -0,0 +1,19 @@
+package com.dksys.biz.user.rd.rd05.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface RD05Mapper {
+
+	int selectRcvpayDailyCount(Map<String, String> paramMap);
+
+	List<Map<String, String>> selectRcvpayDailyList(Map<String, String> paramMap);
+
+	int selectRcvpayDailyDtlCount(Map<String, String> paramMap);
+
+	List<Map<String, String>> selectRcvpayDailyDtlList(Map<String, String> paramMap);
+
+}
---
+++ src/main/java/com/dksys/biz/user/rd/rd05/service/impl/RD05SvcImpl.java
@@ -0,0 +1,39 @@
+package com.dksys.biz.user.rd.rd05.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dksys.biz.user.rd.rd05.mapper.RD05Mapper;
+import com.dksys.biz.user.rd.rd05.service.RD05Svc;
+
+@Service
+@Transactional("erpTransactionManager")
+public class RD05SvcImpl implements RD05Svc {
+	
+    @Autowired
+    RD05Mapper rd05Mapper;
+    
+	@Override
+	public int selectRcvpayDailyCount(Map<String, String> paramMap) {
+		return rd05Mapper.selectRcvpayDailyCount(paramMap);
+	}
+
+	@Override
+	public List<Map<String, String>> selectRcvpayDailyList(Map<String, String> paramMap) {
+		return rd05Mapper.selectRcvpayDailyList(paramMap);
+	}
+    
+	@Override
+	public int selectRcvpayDailyDtlCount(Map<String, String> paramMap) {
+		return rd05Mapper.selectRcvpayDailyDtlCount(paramMap);
+	}
+
+	@Override
+	public List<Map<String, String>> selectRcvpayDailyDtlList(Map<String, String> paramMap) {
+		return rd05Mapper.selectRcvpayDailyDtlList(paramMap);
+	}
+}
