bd55daf5edd4b95b62f9f92a7685799febd77fb9
seonggudk <seonggu.park@dongkuk.com>
2021-06-18 16:31:38 +0900

    재고관리 > 재고 조회(요약) 페이지 추가

---
+++ src/main/java/com/dksys/biz/user/sm/sm07/mapper/SM07Mapper.java
@@ -0,0 +1,19 @@
+package com.dksys.biz.user.sm.sm07.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SM07Mapper {
+	
+	int selectStockSummaryListCount(Map<String, String> param);
+	
+	List<Map<String, String>> selectStockSummaryList(Map<String, String> param);
+	
+	int selectStockSummaryDetailListCount(Map<String, String> param);
+	
+	List<Map<String, String>> selectStockSummaryDetailList(Map<String, String> param);
+	
+}
