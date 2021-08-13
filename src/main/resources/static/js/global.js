90f8d82e84b3695d0cd4001490439591411d69eb
jagnsubnam <firefly7@korea.com>
2021-08-13 15:14:16 +0900

    회사코드 선태가능(감리용)

--- src/main/resources/static/js/global.js
+++ src/main/resources/static/js/global.js
@@ -891,7 +891,7 @@ function authChk(menuUrl){
 	});
 	
 //	// select 회사코드 disable (감사용 임시코드)
-//	$('select[data-kind="CO"]').prop("disabled", true);
+//	$('select[data-kind="CO"]').prop("disabled", true); 
 }
 
 // 버튼 컨펌
