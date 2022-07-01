b41ad4826da500d3bccc3d3f0e61b717916d2ad5
alsdl118 <alsdl118@naver.com>
2022-07-01 16:29:05 +0900

    잡손실 /잡이익 관련 로직 추가/  계정코드 누락 버그 수정

--- src/main/java/com/dksys/biz/douzone/dz/dz01/service/impl/DZ01SvcImpl.java
+++ src/main/java/com/dksys/biz/douzone/dz/dz01/service/impl/DZ01SvcImpl.java
@@ -55,8 +55,12 @@ public class DZ01SvcImpl implements DZ01Svc {
 	@Override
   	public int  dzInsert(Map<String, String> paramMap){
 	
-	  //---------------------------1차 INSERT--------------------------------------------------------------------------
-    	
+	/*
+	 * 1.초기 데이터 셋을 생성한다  mapping table 이 한정적 이기 때문에 코딩으로 구분을 해서 만든 코드값이 많음 
+	 * 2. for 문을 돌려 차변 대변 셋팅을 한다 
+	 * 3. 추후에  2개의 행이 아니라 다른 행을 사용할 시 생성 구분 table 을 만들어 관리하면 더욱더 용이 
+	 * 4. 금문에서 사용하는 전표의 종류는 부가세가 없는 입출금 내역 뿐이라서 2줄로 마무리 할 수 있음     	
+	 */
       String etrdpsAmt = paramMap.get("etrdpsAmt");
     
       int in_sq = 0 ;		             // 거래순번
@@ -90,30 +94,77 @@ public class DZ01SvcImpl implements DZ01Svc {
        *   %상계 (지급보증) 일때 위차면 금액 -  대변 아래쪽도 차변 처리 ( 대변 없음 ) 상계 지급 보증 코드 =ETRDPS04
        */
       
-      String etrdps_type = paramMap.get("etrdpsTyp");
+      String etrdps_type = paramMap.get("etrdpsTyp");   //지급 구분 
+      String msclPrftYn  = paramMap.get("msclPrftYn");  //잡손실 잡이득 유무 컬럼  
+      String dzCode      = paramMap.get("dzCode");		//dzCode  
       
       for (int i = 1 ; i <=ln_sq; i++) {
     	  
     	  	if( i == 1 ) { 	// 차변 전표 생성 
-    	  		
-    	  		
     	  			
-	    	  		 if (setleTypCd.equals("PMNTMTD02")) { //어음 일때 데이터 셋팅 
-	    	  	    	 
-	    	  			paramMap.put("dzCode","11000"); 				//어음일때 어음 코드 셋팅 
-	    	  			paramMap.put("frDt",paramMap.get("etrdpsDt"));  //어음일때 시작일 
-	    	  			paramMap.put("toDt",paramMap.get("exprtnDt"));  //어음일때 만기일 
-	    	  			paramMap.put("ctNb",paramMap.get("bilNo"));     //어음일때 어음번호 셋팅 
-	    	  			
-	    	  	     }else if (etrdps_type.equals("ETRDPS04")) { //상계 지급 보충일때는 차변 값 - 값으로 전환 
-	    	  	    	 
-	    	  	    	int etr_amt =Integer.parseInt(paramMap.get("etrdpsAmt"))*-1;  //string  int parse 음수 전환 
-	    	  	    	paramMap.put("etrdpsAmt", Integer.toString(etr_amt));   
-	    	  	    	
-	    	  	     }
+		    	  		 if (setleTypCd.equals("PMNTMTD02")) { //어음 일때 데이터 셋팅 
+		    	  	    	 
+		    	  			paramMap.put("dzCode","11000"); 				//어음일때 어음 코드 셋팅 
+		    	  			paramMap.put("frDt",paramMap.get("etrdpsDt"));  //어음일때 시작일 
+		    	  			paramMap.put("toDt",paramMap.get("exprtnDt"));  //어음일때 만기일 
+		    	  			paramMap.put("ctNb",paramMap.get("bilNo"));     //어음일때 어음번호 셋팅 
+		    	  			paramMap.put("drcrFg", "3");    
+		    	  			
+		    	  	     }else if (etrdps_type.equals("ETRDPS04")) { //상계 지급 보충일때는 차변 값 - 값으로 전환 
+		    	  	    	 
+		    	  	    	int etr_amt =Integer.parseInt(paramMap.get("etrdpsAmt"))*-1;  //string  int parse 음수 전환 
+		    	  	    	paramMap.put("etrdpsAmt", Integer.toString(etr_amt));   
+		    	  	    	paramMap.put("drcrFg", "3");    
+		    	  	    	
+		    	  	     }else if(msclPrftYn!=null) {
+		    	  	    	 
+		    	  		      if (msclPrftYn.equals("N")&&dzCode.equals("25100")) { 
+				    	  	    	/*
+					    	  	    	매입일때 손실이 방생했을시 매입 코드 25100 
+					    	  	    	구분코드 : 차변/차변
+					    	  	    	계정코드 : 외상매입금 25100 / 잡손실 96000
+					    	  	    	금액      :  -/ + 
+				
+					    	  	    	매입일때 잡이익이 발생했을시 
+					    	  	    	구분코드 : 차변/대변
+					    	  	    	계정코드 : 외상매입금 25100  / 잡이익 93000
+					    	  	    	금액      : + / + 
+				    	  	    	 */
+				    	  	    	int etr_amt =Integer.parseInt(paramMap.get("etrdpsAmt"))*-1;  //string  int parse 음수 전환 
+				    	  	    	paramMap.put("etrdpsAmt", Integer.toString(etr_amt));  
+				    	  	    	paramMap.put("drcrFg", "3");    
+				    	  	    	
+				    	  	     }else if(msclPrftYn.equals("Y")&& dzCode.equals("10800")) {
+				    	  	    	/*
+										매출일때 잡이익이 발생했을시 
+										구분코드 : 대변/대변
+										계정코드 :  외상매출금 10800  /잡이익 93000
+										금액      : - /+
+										
+										매출일때 잡손실이 발생했을시 
+										구분코드 : 차변/대변
+										계정코드 : 외상매출금 10800  / 잡손실 96000
+										금액      : +/+
+									*/
+					    	  	   	int etr_amt =Integer.parseInt(paramMap.get("etrdpsAmt"))*-1;  //string  int parse 음수 전환 
+				    	  	    	paramMap.put("etrdpsAmt", Integer.toString(etr_amt));   
+				    	  	        paramMap.put("drcrFg", "4");      //대변 처리 
+				    	  	    	 
+				    	  	    }else {
+				    	  	    	
+				    	  	    	paramMap.put("drcrFg", "3");    
+				    	  	    	
+				    	  	    }
+		    	  	    	 
+		    	  	    	 
+		    	  	     }else {// 일반적인 데이터 
+		    	  	    	 
+		    	  	    	paramMap.put("drcrFg", "3");    
+		    	  	    	
+		    	  	     }
     	  		
     	  			paramMap.put("lnSq", Integer.toString(i));
-    	  			paramMap.put("drcrFg", "3");    	  			
+    	  			  			
     	  			
     	  	}else {  	// 대변데이터 생성  
     	  			 	// 은행지급 계좌가 있을시  거래처 제외    	  		
@@ -121,45 +172,96 @@ public class DZ01SvcImpl implements DZ01Svc {
 	    	  				
 	    	  			}else {
 	    	  				
-	    	  					paramMap.put("crn","");     	// 은행계좌가 있을시 사업자 코드 NULL
-		    	  			    paramMap.put("trCd",tr_cd); 	// 은행 거래처 코드 넣어줌 	 
+	    	  				paramMap.put("crn","");     	// 은행계좌가 있을시 사업자 코드 NULL
+		    	  			paramMap.put("trCd",tr_cd); 	// 은행 거래처 코드 넣어줌 	 
 	    	  					 
 	    	  			}
 	    	  			
-    	  		    paramMap.put("lnSq", Integer.toString(i));
+	    	  				paramMap.put("lnSq", Integer.toString(i));
     	  		    
-    	  		    if (etrdps_type.equals("ETRDPS04")) {	 //상계 지급 보충일때
-    	  		    	
-        	  		    paramMap.put("drcrFg", "3");         //차변 셋팅 
-        	  		    paramMap.put("dzCode","10300"); 	 //보통 예금 
-        	  		 	int etr_amt =Integer.parseInt(paramMap.get("etrdpsAmt"))*-1;  //string  int parse 양수전환
-	    	  	    	paramMap.put("etrdpsAmt", Integer.toString(etr_amt));   
-	    	  	    	
-    	  		    }else {  //일반 적인 데이터 
-    	  		    	
-        	  		    paramMap.put("drcrFg", "4");		 //대변 셋팅 
-        	  		    String dztemp=  paramMap.get("dzCode2");  //대변 dzCode2 넣어줌 
-        	  		    paramMap.put("dzCode",dztemp);
-        	  		    
-    	  		    }
-    	  		    
+	    	  		    if (etrdps_type.equals("ETRDPS04")) {	 //상계 지급 보충일때
+	    	  		    	
+	        	  		    paramMap.put("drcrFg", "3");         //차변 셋팅 
+	        	  		    paramMap.put("dzCode","10300"); 	 //보통 예금 
+	        	  		 	int etr_amt =Integer.parseInt(paramMap.get("etrdpsAmt"))*-1;  //string  int parse 양수전환
+		    	  	    	paramMap.put("etrdpsAmt", Integer.toString(etr_amt));   
+		    	  	    	
+	    	  		    }else if (msclPrftYn!=null) {// 잡손실 값이 null 일때 건너뜀  equals 로 비교시 예외 발생 
+	    	  		    	
+	    	  		    	if (msclPrftYn.equals("N")&&dzCode.equals("25100")){ //차변 / 차변이 들어가는 경우 
+	    	    	  		    
+	    	    	  		  	/*
+	    		    	  	    	매입일때 손실이 방생했을시 매입 코드 25100 
+	    		    	  	    	구분코드 : 차변/차변
+	    		    	  	    	계정코드 : 외상매입금 25100 / 잡손실 96000
+	    		    	  	    	금액      :  -/ + 
+	    	
+	    		    	  	    	매입일때 잡이익이 발생했을시 
+	    		    	  	    	구분코드 : 차변/대변
+	    		    	  	    	계정코드 : 외상매입금 25100  / 잡이익 93000
+	    		    	  	    	금액      : + / + 
+	    	    	  	    	 */
+	    		    	  		    paramMap.put("drcrFg", "3");		 //차변 셋팅 
+	    		        	  		paramMap.put("dzCode","96000");	 //잡손실 코드 셋팅 
+	    		        	  	   	int etr_amt =Integer.parseInt(paramMap.get("etrdpsAmt"))*-1;  //string  int parse 음수 전환 
+				    	  	    	paramMap.put("etrdpsAmt", Integer.toString(etr_amt));   
+				    	  	    	
+	    	    	  		    }else if(msclPrftYn.equals("Y")&& dzCode.equals("10800")) {// 매출일때 차변차변이 들어간 경우 
+	    	    	  		   	/*
+	    							매출일때 잡이익이 발생했을시 
+	    							구분코드 : 차변/차변 
+	    							계정코드 :  외상매출금 10800  /잡이익 93000
+	    							금액      : - /+
+	    							
+	    							매출일때 잡손실이 발생했을시 
+	    							구분코드 : 차변/대변
+	    							계정코드 : 외상매출금 10800  / 잡손실 96000
+	    							금액      : +/+
+	    						*/
+	    			        	  	paramMap.put("dzCode","93000");		 //잡손실 코드 셋팅 
+	    			        	   	int etr_amt =Integer.parseInt(paramMap.get("etrdpsAmt"))*-1;  //string  int parse 음수 전환 
+				    	  	    	paramMap.put("etrdpsAmt", Integer.toString(etr_amt));   
+	    	    	  		    	
+	    	    	  		    }else {
+	    	    	  		    	
+	    	    	  		      paramMap.put("drcrFg", "4");	
+	    	    	  		      
+		    	    	  		    if(msclPrftYn!=null ) {//잡손실/이익 부분 계정 코드 셋팅 
+		    	        	  		    
+		 		        	  		   if (msclPrftYn.equals("N")) {  //잡이익 - 잡손실  계정코드 셋팅 
+		 		        	  		    	
+		 		        	  				paramMap.put("dzCode","96000");	//잡손실
+		 		        	  		    	
+		 		        	  		   }else if (msclPrftYn.equals("Y")) {
+		 		        	  			   
+		 		        	  				paramMap.put("dzCode","93000");	//잡이익 
+		 		        	  				
+		 		        	  		   }
+		 	        	  		    }
+		    	    	  		    	
+	    	    	  		    }
+	    	  		    	
+	    	  		    }else {  //위 조건 제외  데이터 
+	    	  		    	
+	        	  		    paramMap.put("drcrFg", "4");			  //대변 셋팅 
+	        	  		    String dztemp=  paramMap.get("dzCode2");  //대변 dzCode2 넣어줌 
+	        	  		    paramMap.put("dzCode",dztemp);
+	        	  		    
+	    	  		    }
     	  		    	
     	  	}
     	  	
-    	   // System.out.println("MAP::::"+paramMap.get("inSq")+"   :    "+paramMap.get("etrdpsDt") );
-    	    
-    	    try {
+    	    try {//insert 실패시 화면 멈춰버림 예외로 넘길수있게 처리 
     	    	
     	     dz01Mapper.dzInsert(paramMap);
     	    	
     	    }catch (Exception e) {
     	    	
     	    	e.printStackTrace();
+    	    	return 30000;  //error 코드 return 
 			}
       }
- 
-      System.out.println("seq:::::::::::::"+etrdpsAmt+"::::::::::::::::::::::::::::::::"+tr_cd);
-    
+      
       
       return in_sq;
   }
@@ -170,7 +272,7 @@ public class DZ01SvcImpl implements DZ01Svc {
     public int dzDelete(Map<String, String> paramMap) { //더존 전표 삭제 
    	//전표 삭제 관련 
     try {
-    		
+    		 
         int chk = dz01Mapper.getDzCnt(paramMap);
 		    	
 	    	if( chk >0 ) {  //chk 가 0 보다 크면 발행된 전표가 있음 
