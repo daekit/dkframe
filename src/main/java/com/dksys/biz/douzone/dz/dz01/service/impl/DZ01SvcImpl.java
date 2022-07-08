b6890cd9cac5f0f7dd4825330215c0165d5265a9
alsdl118 <alsdl118@naver.com>
2022-07-08 11:21:31 +0900

    다중선택 발행 추가

--- src/main/java/com/dksys/biz/douzone/dz/dz01/service/impl/DZ01SvcImpl.java
+++ src/main/java/com/dksys/biz/douzone/dz/dz01/service/impl/DZ01SvcImpl.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dksys.biz.admin.cm.cm06.service.CM06Svc;
@@ -15,7 +14,7 @@ import com.dksys.biz.douzone.dz.dz01.mssqlmapper.DZ01Mapper;
 import com.dksys.biz.douzone.dz.dz01.service.DZ01Svc;
 
 @Service
-@Transactional(value="mssqlTransactionManager",rollbackFor = Exception.class, propagation = Propagation.REQUIRES_NEW , isolation = Isolation.SERIALIZABLE)
+@Transactional(value="mssqlTransactionManager",rollbackFor = Exception.class)
 public class DZ01SvcImpl implements DZ01Svc {
 	@Autowired
 	DZ01Mapper dz01Mapper;
